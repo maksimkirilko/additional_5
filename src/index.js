e497bb7895a739e9ccb94611d464003b7651e7c1
Maksim Kirylka <maksimkirylka@gmail.com>
2017-09-23 13:18:29 +0300

    Implement function that for given brackets sequence will return true if it is correct and false otherwise

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = (str, bracketsConfig) => {
+    if (str.length % 2 !== 0) return false;
+    const box = [];
+    str.split('').forEach(char => {
+        bracketsConfig.forEach((bracket, index) => {
+            if (bracket[0] === bracket[1]) {
+                if (bracket[0] === char) addOrRemove(box, index);
+            } else {
+                if (bracket[0] === char) box.push(index);
+                if (bracket[1] === char) addOrRemove(box, index);
+            }
+        });
+    });
+    return box.length === 0;
+};
+
+const addOrRemove = (box, index) => {
+    box[box.length - 1] === index ?
+        box.splice(box.length - 1, 1) : box.push(index);
+};
